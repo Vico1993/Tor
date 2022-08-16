8878f21a30daecc6f9d4ef4f6dd09d5479921ce2
Victor Piolin <victor.piolin@gmail.com>
2022-08-15 22:43:21 -0400

    On and off command Still testing API

--- domain/handlers/update.go
+++ domain/handlers/update.go
@@ -1,26 +1,38 @@
 package handlers
 
 import (
+	"github.com/Vico1993/Tor/domain/hue"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 // We are checking if the update we receive is a Message
 // From a Group
 // if it's a Command
-func shouldReact(update tgbotapi.Update) bool {
-	return update.Message == nil &&
-		update.FromChat().Type != "group" &&
-		!update.Message.IsCommand()
+func shouldNotReact(update tgbotapi.Update) bool {
+	return update.FromChat().Type != "group" || update.Message == nil || !update.Message.IsCommand()
 }
 
 func HandleUpdate(update tgbotapi.Update, bot *tgbotapi.BotAPI) {
-	if !shouldReact(update) {
+	if shouldNotReact(update) {
 		// TODO: Log this why we don't act upon it
 		return
 	}
 
 	// Create default message
 	msg := tgbotapi.NewMessage(update.Message.Chat.ID, "I didn't understand...")
+
+	switch update.Message.Command() {
+		case "help":
+			msg.Text = "I understand /sayhi and /status."
+		case "off":
+			msg.Text = "Light is coming"
+			hue.SetLight(false)
+		case "on":
+			msg.Text = "let's go to bed!"
+			hue.SetLight(true)
+		default:
+			msg.Text = "I don't know that command"
+	}
 
 	if _, err := bot.Send(msg); err != nil {
 		panic(err)
---
+++ domain/hue/init.go
@@ -0,0 +1,81 @@
+package hue
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/heatxsink/go-hue/groups"
+	"github.com/heatxsink/go-hue/lights"
+)
+
+/*
+	l := lightAPI.New(os.Getenv("HUE_TEST_HOSTNAME"), os.Getenv("HUE_TEST_USERNAME"))
+	lights, err := l.GetAllLights()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Number of lights", len(lights))
+
+	for _, light := range lights {
+		fmt.Println(light.ID,light.Name)
+
+		if light.ID == 14 {
+			res, err := l.SetLightState(light.ID, lightAPI.State{
+				On: false,
+				TransitionTime: 1000,
+			})
+			if err != nil {
+				panic(err)
+			}
+
+			fmt.Println(res)
+		}
+	}
+*/
+
+func SetLight(on bool) {
+	gg := groups.New(os.Getenv("HUE_TEST_HOSTNAME"), os.Getenv("HUE_TEST_USERNAME"))
+	grps, err := gg.GetAllGroups()
+	if err != nil {
+		panic(err)
+	}
+
+	var bri uint8 = 100
+	if !on {
+		bri = 0
+	}
+
+	for _, grp := range grps {
+		fmt.Println(grp.ID, grp.Name)
+
+		if (grp.ID == 5) {
+			gg.SetGroupState(grp.ID, lights.State{
+				On: on,
+				Bri: bri,
+				TransitionTime: 1,
+			})
+		}
+	}
+}
+
+// func ShutDownLight() {
+// 	fmt.Println("Shutting down...")
+
+// 	gg := groups.New(os.Getenv("HUE_TEST_HOSTNAME"), os.Getenv("HUE_TEST_USERNAME"))
+// 	grps, err := gg.GetAllGroups()
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	for _, grp := range grps {
+// 		fmt.Println(grp.ID, grp.Name)
+
+// 		if (grp.ID == 5) {
+// 			gg.SetGroupState(grp.ID, lights.State{
+// 				On: false,
+// 				TransitionTime: 1,
+// 			})
+// 		}
+// 	}
+// }
