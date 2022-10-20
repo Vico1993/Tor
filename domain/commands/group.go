e2e27d51b283a481b1efe8f06bd4c1c18d23bac5
Victor Piolin <victor.piolin@gmail.com>
2022-10-19 23:40:29 -0400

    Add basic On Off support for group command

--- domain/commands/group.go
+++ domain/commands/group.go
@@ -2,7 +2,9 @@ package commands
 
 import (
 	"fmt"
+	"strings"
 
+	"github.com/Vico1993/Tor/domain/hue"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/heatxsink/go-hue/groups"
 )
@@ -13,6 +15,33 @@ type groupCmd struct {
 }
 
 func (cmd *groupCmd) Exec(bot *tgbotapi.BotAPI, chatId int64, params string) error {
+	if params == "" {
+		// Return help command for this group
+		return nil
+	}
+
+	sliceParams := strings.Split(params, " ")
+
+	if sliceParams[0] == "" {
+		return nil
+	}
+
+	// First params need to be scene name or on/off
+	if sliceParams[0] == "on" {
+		hue.PowerUpGroup(cmd.group.Name)
+		return nil
+	}
+
+	if sliceParams[0] == "off" {
+		hue.ShutDownGroup(cmd.group.Name)
+		return nil
+	}
+
+	// Check scene for the group
+
+	// Second is either not set
+	// If not set set it to 100% or 0
+
 	fmt.Println("Group Command!")
 	fmt.Println("Here are the parameters:", params)
 
